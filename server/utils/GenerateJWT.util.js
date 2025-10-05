cee77fd27c19e0e7b65c4813ac6a86ddcd775dd3
Dhanush N <mrcoyote2075@gmail.com>
2025-10-05 17:47:44 +0530

    updated Cookie config

--- server/utils/GenerateJWT.util.js
+++ server/utils/GenerateJWT.util.js
@@ -2,15 +2,17 @@ import jwt from "jsonwebtoken";
 
 export const GenerateJwtTokens = (data, res) => {
     try {
-        // Token...
         const token = jwt.sign(data, process.env.JWT_SECRET_KEY, {
             expiresIn: "7d",
         });
-        // Cookie...
+
+        const isProduction = process.env.STATUS !== 'development';
+
         res.cookie(process.env.JWT_TOKEN_NAME, token, {
-            maxAge: 7 * 24 * 60 * 60 * 1000, // weeks * days * hours * mins * secs * millis ...
-            httpOnly: true, //url secure...
-            secure: process.env.STATUS != 'development', // protocol security... [https]
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+            httpOnly: true,
+            secure: isProduction, 
+            sameSite: isProduction ? 'none' : 'lax', 
         });
 
         return token;
